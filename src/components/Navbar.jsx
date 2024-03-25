bf4846f0bd8975808a9d4b1b9fa8dc3715f00184
LAPTOP-AN9TJOK7\kankichi.jibiki <kankichi.jibiki@sola-air.com>
2024-03-25 18:47:39 +0900

    header in main page sample done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-    const linkClass = ({ isActive }) => isActive ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-lg';
+    const linkClass = ({ isActive }) => isActive ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-md';
 
     return (
-    <nav className='bg-teal-500 border-b border-indigo-300'>
+    <nav className='bg-teal-500 border-b border-teal-300'>
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="flex h-20 items-center justify-between">
+            <div className="flex h-16 items-center justify-between">
                 <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                     {/* left nav */}
                     <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
@@ -16,11 +16,6 @@ const Navbar = () => {
                     {/* right nav */}
                     <div className="md:ml-auto">
                         <div className="flex space-x-1">
-                            <NavLink
-                            to="/"
-                            className={linkClass}
-                            >Home
-                            </NavLink>
                             <NavLink
                             to="/login"
                             className={linkClass}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
+import MainPage from '../pages/MainPage'
 
 const Routes = () => {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path='/' element={<MainLayout />}>
-                
+                <Route index element={<MainPage />}></Route>
             </Route>
         )
     )
